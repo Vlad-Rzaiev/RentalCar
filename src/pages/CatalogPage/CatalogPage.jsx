626489cd69206a0eeb5725bea2de10bcafc3b108
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-07-25 10:54:46 +0200

    Update CatalogPage.jsx

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -48,7 +48,6 @@ export const CatalogPage = () => {
             value=""
             label="Car brand"
             placeholder="Choose a brand"
-{/*             options={} */}
           />
 
           <CarsList />
